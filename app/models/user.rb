84b32a4d6ddeaf7716bf37c5d031b15a3ce99602
naaa0 <lil.nuts70@gmail.com>
2022-10-15 16:25:24 +0900

    RSpec導入とuserモデルの単体テストコードの記載

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, :lastname, :firstname, :lastname_kana, :firstname_kana, :birthday, presence: true
-  
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
 
   validates :firstname, :lastname, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' }
-  validates :lastname_kana, :firstname_kana, format: { with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/, message: "全角カタカナのみで入力して下さい"}
-
+  validates :lastname_kana, :firstname_kana, format: { with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/, message: '全角カタカナのみで入力して下さい' }
 end
