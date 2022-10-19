4fd3ab2a85df91886bbaed6d3d0c74ca6d88f3c8
naaa0 <lil.nuts70@gmail.com>
2022-10-19 15:53:13 +0900

    カテゴリー選択機能の実装(ActiveHash)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,17 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:category_id, :condition_id, :delivery_bearer_id, :region_id, :delivery_date_id)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,15 +1,21 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery_bearer
+  belongs_to :region
+  belongs_to :delivery_date
 
   with_options presence: true do
     validates :name
     validates :description
-    validates :category_id
-    validates :condition_id
-    validates :delivery_bearer_id
-    validates :region_id
-    validates :delivery_date_id
+    validates :category_id,        numericality: { other_than: 1 , message: "can't be blank"}
+    validates :condition_id,       numericality: { other_than: 1 , message: "can't be blank"}
+    validates :delivery_bearer_id, numericality: { other_than: 1 , message: "can't be blank"}
+    validates :region_id,          numericality: { other_than: 1 , message: "can't be blank"}
+    validates :delivery_date_id,   numericality: { other_than: 1 , message: "can't be blank"}
     validates :price, format: { with:  /\A[0-9]+\z/ },
                       numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
   end
