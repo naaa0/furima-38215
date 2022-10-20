86fa36b2e8a3d3d16ea319ca3e90f2b1cef5598e
naaa0 <lil.nuts70@gmail.com>
2022-10-20 23:59:08 +0900

    itemモデルの単体テストコード作成

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,95 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品を出品する' do
+    context '商品を出品できる場合' do
+      it '必要事項を全て過不足なく入力すると出品できる' do
+        expect(@item).to be_valid
+      end
+    end
+    context '出品できない場合' do
+      it 'imageが空では登録できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+
+      it 'nameが空では登録できない' do
+        @item.name = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
+      end
+
+      it 'discriptionが空では登録できない' do
+        @item.description = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Description can't be blank")
+      end
+
+      it 'category_idが空では登録できない' do
+        @item.category_id = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+
+      it 'condition_idが空では登録できない' do
+        @item.condition_id = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Condition can't be blank")
+      end
+
+      it 'delivery_bearer_idが空では登録できない' do
+        @item.delivery_bearer_id = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery bearer can't be blank")
+      end
+
+      it 'region_idが空では登録できない' do
+        @item.region_id = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Region can't be blank")
+      end
+
+      it 'delivery_date_idが空では登録できない' do
+        @item.delivery_date_id = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery date can't be blank")
+      end
+
+      it 'priceが空では登録できない' do
+        @item.price = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+
+      it 'priceが自然数でないと登録できない' do
+        @item.price = 333.333
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be an integer")
+      end
+
+      it 'priceが300未満では登録できない' do
+        @item.price = Faker::Number.between(from: 0, to: 299)
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+      end
+
+      it 'priceが9999999より多いと登録できない' do
+        @item.price = 10000000
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
+      end
+
+      it 'Userが紐づいていないと登録できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("User must exist")
+      end
+    end
+  end
 end
+
