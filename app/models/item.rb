4542c6d6e4aadab9e1118b8c81797fc9334ba99a
naaa0 <lil.nuts70@gmail.com>
2022-10-20 18:29:24 +0900

    バリデーションの追加(imageカラム)

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :condition
@@ -9,6 +10,7 @@ class Item < ApplicationRecord
   belongs_to :delivery_date
 
   with_options presence: true do
+    validates :image
     validates :name
     validates :description
     validates :category_id,        numericality: { other_than: 1 , message: "can't be blank"}
@@ -19,4 +21,5 @@ class Item < ApplicationRecord
     validates :price, format: { with:  /\A[0-9]+\z/ },
                       numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
   end
+
 end
