bac45a5a072f7699c540551d5bddb79539050723
naaa0 <lil.nuts70@gmail.com>
2022-10-20 18:33:52 +0900

    itemsコントローラーの編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,11 +7,16 @@ class ItemsController < ApplicationController
   end
 
   def create
-    Item.create(item_params)
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render action: :new
+    end
   end
 
   private
   def item_params
-    params.require(:item).permit(:category_id, :condition_id, :delivery_bearer_id, :region_id, :delivery_date_id)
+    params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :delivery_bearer_id, :region_id, :delivery_date_id, :price).merge(user_id: current_user.id)
   end
 end
