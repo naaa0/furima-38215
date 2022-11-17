bae9461e9d304fee9f792f44c301440071334d0f
naaa0 <lil.nuts70@gmail.com>
2022-11-17 16:31:00 +0900

    自動デプロイの実装

---
+++ config/deploy.rb
@@ -0,0 +1,25 @@
+lock '3.17.1'
+
+set :application, 'furima-38215'
+
+set :repo_url,  'git@github.com:naaa0/furima-38215.git'
+
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
+set :rbenv_type, :user
+set :rbenv_ruby, '2.6.5'
+
+set :ssh_options, auth_methods: ['publickey'],
+                                  keys: ['~/.ssh/furima38215key.pem'] 
+
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :keep_releases, 5
+
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
---
+++ config/deploy/production.rb
@@ -0,0 +1,1 @@
+server '54.65.150.79', user: 'ec2-user', roles: %w{app db web}
